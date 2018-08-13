aa06137c3caf037df0c2b27cde25db2c62a20fff
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-08-12 21:06:54 -0700

    Major GWT changes to Mover32
    This also significantly improves its period.

--- src/main/java/sarong/Mover32RNG.java
+++ src/main/java/sarong/Mover32RNG.java
@@ -5,16 +5,18 @@ import sarong.util.StringKit;
 /**
  * One of Mark Overton's subcycle generators from <a href="http://www.drdobbs.com/tools/229625477">this article</a>,
  * specifically a cmr^cmr with two 32-bit states; this is the fastest 32-bit generator that still passes statistical
- * tests, plus it's optimized for GWT. It has a period of slightly more than 2 to the 63.86, 0xE89BB7902049CD38, and
- * allows 2 to the 32 initial seeds.
+ * tests, plus it's optimized for GWT. It has a period of just under 2 to the 64, 0xFFF547E3D0AA1626, which is 2 to the
+ * 63.999764, and allows 2 to the 32 initial seeds.
  * <br>
- * This seems to do well in PractRand testing, up to at least 16TB with nothing worse than "unusual", and at least some
- * variants on cmr+cmr pass BigCrush according to Overton. "Chaotic" generators like this one tend to score well in
- * PractRand, but it isn't clear if they will fail other tests. As for speed, this is faster than {@link Lathe32RNG}
- * (which is also high-quality) and is also faster than {@link XoRo32RNG} (which is very fast but has quality issues)
- * and {@link ThrustAlt32RNG} (which has a very small period and probably isn't very useful). Its period is
- * 0xE89BB7902049CD38 or 16,761,192,267,834,314,040 for the largest cycle, which it always initializes into if
- * {@link #setState(int)} is used. setState() only allows 2 to the 32 starting states, but less than 2 to the 64 states
+ * This seems to do well in PractRand testing (testing hasn't finished on this generator, but results up to 64GB have no
+ * anomalies or failures), but this is not one of the exact generators Overton tested. "Chaotic" generators like this
+ * one tend to score well in PractRand, but it isn't clear if they will fail other tests. As for speed, this is faster
+ * than {@link Lathe32RNG} (which is also high-quality) and is also faster than {@link XoRo32RNG} (which is very fast
+ * but has quality issues) and {@link ThrustAlt32RNG} (which has a very small period and probably isn't very useful).
+ * A similar generator (One of Overton's CMR^CMR type) is even faster on desktop, but is slower than Lathe32RNG when
+ * using GWT and viewing in Firefox or Chrome.
+ * Its period is 0xFFF547E3D0AA1626 for the largest cycle, which it always initializes into if {@link #setState(int)} is
+ * used. setState() only allows 2 to the 32 starting states, but less than 2 to the 64 states
  * are in the largest cycle, so using a long or two ints to set the state seems ill-advised.
  * <br>
  * This is a RandomnessSource but not a StatefulRandomness because it needs to take care and avoid seeds that would put
@@ -51,69 +53,88 @@ public final class Mover32RNG implements RandomnessSource {
     }
 
     private static final int[] startingA = {
-            0x00000001, 0x9F11DF0B, 0xA9D75498, 0xF4B421DE, 0xB9A82430, 0xED4250C8, 0x15E93F42, 0xDB18983F,
-            0x4932B222, 0x312D3B0D, 0x06EE5612, 0xA18E8AF5, 0xC35ED6EC, 0x2296C3BB, 0x897A99B6, 0x113314FA,
-            0xE5DC6DF1, 0xA1CB5430, 0x88F7B7A7, 0x26681BF6, 0xA0823E05, 0x37F881AC, 0x6535F6E5, 0x97B3DE1B,
-            0xA7EBDF98, 0x8783572B, 0x2C15E0B3, 0x80B297D7, 0x4C4A107F, 0xB27C96F2, 0x304FF465, 0x5C3FB23E,
-            0xBB935E48, 0x6E618A42, 0x080A85CE, 0x352770AF, 0xB3E8D627, 0x68B3AA7C, 0x1F11C1F2, 0x7A686342,
-            0x517FFE3F, 0x92843302, 0x975D9502, 0xB07AE92C, 0x803D9C7B, 0x60AC7206, 0x1D081127, 0xC143F147,
-            0x6A04EB07, 0x2A943751, 0x92C44902, 0xD445ACAF, 0xB85ED97D, 0x08D9E51D, 0x76AFE840, 0xB616A31B,
-            0x2CC4B653, 0x7D2D0367, 0x894DBD23, 0xB685E564, 0xE79CD06E, 0xA4A39079, 0x9E668C50, 0x7A6FA73F,
-            0x46E9DE96, 0x31603264, 0xFFBEFF3A, 0x2CC114A3, 0xC8595749, 0xA9086FBA, 0x99B297E6, 0xF1A90452,
-            0xC9C0AE15, 0x69CDAED1, 0xAB41467E, 0xD1B6D159, 0xD85FA961, 0xF31455D4, 0x104BD460, 0xF8881A1F,
-            0x51E9EA5D, 0x53EA0817, 0xAAE7751F, 0x62059A86, 0x3CE2F8D4, 0xC786C03D, 0x4987ED94, 0x5A3003F5,
-            0x5B007518, 0x65064757, 0xC235E766, 0x1B9ADC61, 0xAE361B43, 0x6863C452, 0x9503D569, 0x6F465A2C,
-            0x6C9372D0, 0x5AF87B87, 0xAD462E2D, 0x4DA7768E, 0x09427461, 0xC70F40EA, 0xF70E648A, 0x85C95AB2,
-            0xA33D25B0, 0x5463C91D, 0x503A631E, 0x2774EF9A, 0x0C56BD16, 0xF7065D36, 0xD69F74A2, 0x80246F1F,
-            0x4FE448E5, 0x79D2D12C, 0x760B2310, 0x8BB27897, 0x7214F867, 0x3278B28D, 0x3CAAB7AB, 0x20482D22,
-            0x48AA7400, 0x8C7865F4, 0x95504AEE, 0x7121E9BC, 0xAB254563, 0x19F96B6B, 0xA04C2CE3, 0x2B6F9021,
+            0x00000001, 0xE0F813FB, 0x3853A562, 0x416A00FB, 0x969733D0, 0x68FC5927, 0x99A19AAE, 0xC4C790CC,
+            0x996A09D2, 0x6914B3DE, 0xB1A25EE1, 0xBCF1B09F, 0x442C6EF5, 0x475E34F6, 0xCC900DB2, 0x07E720B0,
+            0x0058393C, 0xA8D71A86, 0xFFA9E99A, 0x2A123AD8, 0x162BA47D, 0x6993B504, 0xAFB932FD, 0xFAD8E139,
+            0x77990180, 0x452C71B3, 0xF265CBCD, 0xC0F7B73D, 0x8A7F9F87, 0x845E4932, 0x8B5D1621, 0xBCAFA92A,
+            0xD6D7A1EC, 0x8E2E18A7, 0x33E41E53, 0xDD326FE3, 0x2F05B4EA, 0xB8F6F3C7, 0x9E3DD946, 0x1F0394CB,
+            0x7F0F299C, 0x61CB8AA1, 0xA9E05E22, 0xBBCD908F, 0xB070DD10, 0x0A2DDBB2, 0xCD0E44D6, 0x0D07CF67,
+            0x6318736A, 0x74851D1A, 0x67403862, 0x7EAF0F09, 0xDCDFDA15, 0xAE41DED5, 0x6C2B2FB6, 0x6C2A7DA7,
+            0x3A469C2E, 0x7B3EA36E, 0x858A16F4, 0x5FE35777, 0x21D4F6AF, 0x8547F196, 0x0D562186, 0x8F88EF25,
+            0x38B8DB7C, 0x5C15CB9D, 0x75F3C219, 0xD0CC6A7B, 0xBBD4DA22, 0x38174193, 0xDEF01557, 0xF8B890C3,
+            0x5CAE3E75, 0xE366F7B7, 0x10BB0CAB, 0xF82D9CB8, 0xB7D312CC, 0x69F60C1C, 0x512E23E0, 0xBFA77DBB,
+            0x8CCC44D0, 0x4A486348, 0x233AC5DD, 0xEFF581CF, 0x51D5EA09, 0x6448830E, 0xFF0B9140, 0xA0F34887,
+            0x92C0F3B8, 0xBBF60BDF, 0xCBE1A1A9, 0x31BF3301, 0xEC6E09A5, 0x87F67486, 0x272E487B, 0x3CEA124F,
+            0x55282704, 0x6157B070, 0x6102419F, 0x58651391, 0x1C305E1A, 0xFE77CF87, 0x6C78B78B, 0xFF22C4C9,
+            0x077E96D6, 0xF37FE662, 0x1B08A31A, 0x9C38F87C, 0x55C56A19, 0xE31AA568, 0x11E35EBE, 0xE7C81D24,
+            0xE6C3D7E0, 0x87A9B50F, 0xB7D3547D, 0x34F0B5C9, 0xC4F93C05, 0xEAC59BB3, 0xDEA348B6, 0x23C56119,
+            0xB76AE1A3, 0x0D8DA124, 0x4927CF73, 0x2580B158, 0x1F731C31, 0x9DEDEC30, 0x12BAFE34, 0x0B139973,
     }, startingB = {
-            0x00000001, 0xAF5FF586, 0xCB5FA621, 0x147FEB79, 0x0D2B4A87, 0xC09CD63C, 0x493DCA72, 0xD388DD6F,
-            0x257C6197, 0xDAFCE14D, 0x2778D97C, 0x9488AF6C, 0xE025E6F9, 0x03F7C4A6, 0x84738348, 0x0F7268E9,
-            0xDCD9215E, 0x3FB0AC46, 0x90C7E3DC, 0x4B347FD0, 0x8FB36FBF, 0x525B05B1, 0x311AD316, 0x12375610,
-            0xB0E92B50, 0x95707ED7, 0x0D02EFC9, 0x00DA55BD, 0x32FECFC2, 0x8FAB5273, 0xAEBEB303, 0x77B03E4C,
-            0x3AA3F9D3, 0x456B44D7, 0x92AFC26E, 0x3E887027, 0x584ED1D3, 0x80DC259B, 0xF3E8D5FA, 0xA7C24C1A,
-            0xC415778E, 0xD7BB449A, 0xFECC2032, 0x962501B8, 0x363117E7, 0xEC5E4BCF, 0xE863B55A, 0x1E9BEBF6,
-            0x430EDA44, 0x0BBCD30F, 0x81AA0B42, 0x48E8D6DB, 0x4E90224B, 0x5C7C2E57, 0x3D755220, 0xF1F529A5,
-            0x64E2BF59, 0x9A80E570, 0xFB851D5E, 0xAAF06CAF, 0xBD18D160, 0x81BAD104, 0xCB7F85A5, 0x05D3FEB2,
-            0x3E7F34B6, 0xE2B69058, 0xACCA0C95, 0xF950057D, 0x8EC11DF4, 0xF2CBDB24, 0x863829BF, 0xAEBCA905,
-            0xD24BDD76, 0xD3D7E317, 0x54829318, 0x52A10C3F, 0xB9B86F0A, 0xFD746569, 0x7DAC4A01, 0x2B9F88E9,
-            0xF3184DB6, 0x50C3A49D, 0x8A22549E, 0x9913C627, 0xEEDCEA6F, 0x9C95D489, 0x7C2A42E7, 0x9E9BD919,
-            0x45ACDC0D, 0xECDEEDD9, 0xA24FCCC9, 0xC195D62D, 0x289864BA, 0x89CBC109, 0x4BC5BD74, 0xB4ED95B4,
-            0x5E3CF4C9, 0x0B3EF952, 0x99828E82, 0xCFDC5F50, 0xD5307F76, 0x6B905E5D, 0x818DB5FD, 0x747C61A8,
-            0x68B48552, 0x32C2352B, 0xC0C1FEAD, 0x63C1C57C, 0x5DE87FA0, 0xA97BE18A, 0x14417618, 0x1C7358F7,
-            0x9A7D8D79, 0xDBD3B474, 0xF0594ACC, 0x9D50AF52, 0x0A618F87, 0x23519C0F, 0xFA9E8AB9, 0xA37FDA83,
-            0x74B0372D, 0x48590DFF, 0xB8430CA4, 0x3B30E4A4, 0xC8946F5C, 0xE5D7681B, 0x4D9590CC, 0x634A5F38,
+            0x00000001, 0xFDE752F6, 0xC84AF5A3, 0xA87499BC, 0xD90270DE, 0xE90ACB23, 0x9257E51C, 0x9EE34DF2,
+            0x6B6145AE, 0xEC75C190, 0xBCC18895, 0x32D10686, 0x317F5535, 0x5A97DDE9, 0x6A49F707, 0x4FD63148,
+            0x31C884C3, 0xEE68C32E, 0xDECE7562, 0x989C6CA0, 0x449BAC70, 0xBFF6415E, 0xEB06F9A5, 0xABB8890F,
+            0x859213D3, 0x7D9C5EB7, 0xCEFB7D21, 0x1054397A, 0x47133437, 0x39EA89DC, 0x57F3FA9B, 0xB7C9825D,
+            0xBF5CF78E, 0x67AD1C2D, 0x841B6434, 0x5E82C45F, 0x97948021, 0xBF76909D, 0xF74020C5, 0x52F504F2,
+            0x6B4466F5, 0x6742D957, 0x9FF19ABC, 0x57104213, 0x39B5F5D1, 0x730E17AB, 0x440F8E1B, 0x3B1D4A42,
+            0x8ED24EBF, 0x7ECE6545, 0x710D59C2, 0x96E768D8, 0x3A33BFAF, 0x775A6C30, 0x5DE6E29C, 0x27B5C59D,
+            0x24630AF2, 0x2F174DBC, 0xBF2D6A4C, 0xD6334C3B, 0xBE53EF43, 0xC1ECD43B, 0x6A60478E, 0x108F9B4A,
+            0xA604530E, 0x4570407F, 0xCC6B423A, 0x47C0C0F9, 0xB09671A7, 0xE9A6BDFD, 0xABBD2751, 0x2524B64F,
+            0x69B20A61, 0x0E696B30, 0x81633930, 0x0006ED93, 0x5C12F794, 0xE82602E0, 0xB1D5EDC2, 0xD31990D7,
+            0xA9F6060E, 0x3C6BFA34, 0xD193C00F, 0x82D7DB5E, 0x82C49F3C, 0x7A771155, 0x8F0D9415, 0x8A17684D,
+            0x2D77E8D6, 0x2913858E, 0xB533A466, 0x8129764F, 0x63162CD2, 0x5F2DD6F6, 0xBF8B497A, 0xB43BE06C,
+            0x98654103, 0x8C28E2AD, 0xDF898920, 0x5D7AA02D, 0x402A4E1F, 0x31CAE12C, 0xA03FB63D, 0x45F0D48A,
+            0xEFB636E4, 0x15BA997E, 0x03BE743D, 0x50C3829B, 0x1995789C, 0x9EB14174, 0x7E0FACE3, 0xEACE464A,
+            0x8FD5E698, 0x921A2C9A, 0xBA254C0A, 0x946AD363, 0x380AAFC3, 0xCEA4C41D, 0x13789C1F, 0xD5F712C3,
+            0x9599AECF, 0x03777BEA, 0xE27AD2AD, 0x17F9E31B, 0xA3AE7641, 0x4A607868, 0x7747EE23, 0x56EB9F40,
     };
 
     public final void setState(final int s) {
         stateA = startingA[s >>> 9 & 0x7F];
         for (int i = s & 0x1FF; i > 0; i--) {
-            stateA = Integer.rotateLeft(stateA * 0x9E37, 17);
+            stateA = stateA - 0x9E3779B9 ^ 0xE541440F;
+            stateA = (stateA << 22 | stateA >>> 10);
         }
         stateB = startingB[s >>> 25];
         for (int i = s >>> 16 & 0x1FF; i > 0; i--) {
-            stateB = Integer.rotateLeft(stateB * 0x4E6D, 14);
+            stateB += 0xC0EF50EB;
+            stateB = (stateB << 7 | stateB >>> 25);
         }
     }
 
     public final int nextInt()
     {
-        return (stateA = Integer.rotateLeft(stateA * 0x9E37, 17)) ^ (stateB = Integer.rotateLeft(stateB * 0x4E6D, 14));
+        int y = stateA - 0x9E3779B9 ^ 0xE541440F;
+        y = (stateA = (y << 22 | y >>> 10));
+        final int x = stateB + 0xC0EF50EB;
+        y ^= (stateB = (x << 7 | x >>> 25));
+        y ^= y << 9;
+        return y ^ y >>> 13;
     }
     @Override
     public final int next(final int bits)
     {
-        return ((stateA = Integer.rotateLeft(stateA * 0x9E37, 17))
-                ^ (stateB = Integer.rotateLeft(stateB * 0x4E6D, 14))) >>> (32 - bits);
+        int y = stateA - 0x9E3779B9 ^ 0xE541440F;
+        y = (stateA = (y << 22 | y >>> 10));
+        final int x = stateB + 0xC0EF50EB;
+        y ^= (stateB = (x << 7 | x >>> 25));
+        y ^= y << 9;
+        return (y ^ y >> 13) >>> (32 - bits);
     }
     @Override
     public final long nextLong()
     {
-        final long t = (stateA = Integer.rotateLeft(stateA * 0x9E37, 17))
-                ^ (stateB = Integer.rotateLeft(stateB * 0x4E6D, 14));
-        return t << 32 ^ ((stateA = Integer.rotateLeft(stateA * 0x9E37, 17))
-                ^ (stateB = Integer.rotateLeft(stateB * 0x4E6D, 14)));
+        int y = stateA - 0x9E3779B9 ^ 0xE541440F;
+        y = (y << 22 | y >>> 10);
+        int x = stateB + 0xC0EF50EB;
+        int z = y ^ (x = (x << 7 | x >>> 25));
+        z ^= z << 9;
+        long t = z ^ z >>> 13;
+        y = y - 0x9E3779B9 ^ 0xE541440F;
+        stateA = (y = (y << 22 | y >>> 10));
+        x += 0xC0EF50EB;
+        z = y ^ (stateB = (x << 7 | x >>> 25));
+        z ^= z << 9;
+        return t << 32 ^ (z ^ z >>> 13);
     }
 
     /**
@@ -226,7 +247,7 @@ public final class Mover32RNG implements RandomnessSource {
 //        System.out.printf("\n0x%016X\n", result.longValue());
 //
 //    }
-    
+
 //    public static void main(String[] args)
 //    {
 //        Mover32RNG m = new Mover32RNG();
@@ -246,4 +267,187 @@ public final class Mover32RNG implements RandomnessSource {
 //        }
 //        System.out.println("};");
 //    }
+
+//    public static void main(String[] args)
+//    {
+//        // A 10 0xC010AEB4
+//        // B 22 0x195B9108
+//        // all  0x04C194F3485D5A68
+//
+//        // A 17 0xF7F87D28
+//        // B 14 0xF023E25B 
+//        // all  0xE89BB7902049CD38
+//
+//
+//        // A11 B14 0xBBDA9763B6CA318D
+//        // A8  B14 0xC109F954C76CB09C
+//        // A17 B14 0xE89BB7902049CD38
+////        BigInteger result = BigInteger.valueOf(0xF7F87D28L);
+////        BigInteger tmp = BigInteger.valueOf(0xF023E25BL);
+////        result = tmp.divide(result.gcd(tmp)).multiply(result);
+////        System.out.printf("0x%016X\n", result.longValue());
+//        // 0x9E37
+//        // rotation 27: 0xEE06F34D
+//        // 0x9E35
+//        // rotation 6 : 0xE1183C3A
+//        // rotation 19: 0xC4FCFC55
+//        // 0x9E3B
+//        // rotation 25: 0xE69313ED
+//        // 0xDE4D
+//        // rotation 3 : 0xF6C16607
+//        // rotation 23: 0xD23AD58D
+//        // rotation 29: 0xC56DC41F
+//        // 0x1337
+//        // rotation 7: 0xF41BD009
+//        // rotation 20: 0xF5846878
+//        // rotation 25: 0xF38658F9
+//        // 0xACED
+//        // rotation 28: 0xFC98CC08
+//        // rotation 31: 0xFA18CD57
+//        // 0xBA55
+//        // rotation 19: 0xFB059E43
+//        
+//        // adding
+//        // 0x9E3779B9
+//        // rotation 2 : 0xFFCC8933
+//        // rotation 7 : 0xF715CEDF
+//        // rotation 25: 0xF715CEDF
+//        // rotation 30: 0xFFCC8933
+//        // 0x6C8E9CF5
+//        // rotation 6 : 0xF721971A
+//        // 0x41C64E6D
+//        // rotation 13: 0xFA312DBF
+//        // rotation 19: 0xFA312DBF
+//        // rotation 1 : 0xF945B8A7
+//        // rotation 31: 0xF945B8A7
+//        // 0xC3564E95
+//        // rotation 1 : 0xFA69E895 also 31
+//        // rotation 5 : 0xF2BF5E23 also 27
+//        // 0x76BAF5E3
+//        // rotation 14: 0xF4DDFC5A also 18
+//        // 0xA67943A3 
+//        // rotation 11: 0xF1044048 also 21
+//        // 0x6C96FEE7
+//        // rotation 2 : 0xF4098F0D
+//        // 0xA3014337
+//        // rotation 15: 0xF3700ABF also 17
+//        // 0x9E3759B9
+//        // rotation 1 : 0xFB6547A2 also 31
+//        // 0x6C8E9CF7
+//        // rotation 7 : 0xFF151D74 also 25
+//        // rotation 13: 0xFD468E2B also 19
+//        // rotation 6 : 0xF145A7EB also 26
+//        // 0xB531A935
+//        // rotation 13: 0xFF9E2F67 also 19
+//        // 0xC0EF50EB
+//        // rotation 07: 0xFFF8A98D also 25
+//        // 0x518DC14F
+//        // rotation 09: 0xFFABD755 also 23 // probably not prime
+//        // 0xA5F152BF
+//        // rotation 07: 0xFFB234B2 also 27
+//        // 0x8092D909
+//        // rotation 10: 0xFFA82F7C also 22
+//        // 0x73E2CCAB
+//        // rotation 09: 0xFF9DE8B1 also 23
+//        // stateB = rotate32(stateB + 0xB531A935, 13)
+//        // stateC = rotate32(stateC + 0xC0EF50EB, 7)
+//        
+//        // subtracting, rotating, and bitwise NOT:
+//        // 0xC68E9CF3
+//        // rotation 13: 0xFEF97E17, also 19 
+//        // 0xC68E9CB7
+//        // rotation 12: 0xFE3D7A2E
+//        
+//        // left xorshift
+//        // 5
+//        // rotation 15: 0xFFF7E000
+//        // 13
+//        // rotation 17: 0xFFFD8000
+//        
+//        // minus left shift, then xor
+//        // state - (state << 12) ^ 0xC68E9CB7, rotation 21: 0xFFD299CB
+//        // add xor
+//        // state + 0xC68E9CB7 ^ 0xDFF4ECB9, rotation 30: 0xFFDAEDF7
+//        // state + 0xC68E9CB7 ^ 0xB5402ED7, rotation 01: 0xFFE73631
+//        // state + 0xC68E9CB7 ^ 0xB2B386E5, rotation 24: 0xFFE29F5D
+//        // sub xor
+//        // state - 0x9E3779B9 ^ 0xE541440F, rotation 22: 0xFFFC9E3E
+//        
+//        
+//        // best power of two:
+//        // can get 63.998 with:
+//        // adding 0x9E3779B9 for A and rotating left by 2
+//        // xorshifting B left by 5 (B ^ B << 5) and rotating left by 15
+//        // can get 63.99 with:
+//        // adding 0x9E3779B9 for A and rotating left by 2
+//        // adding 0x6C8E9CF7 for B and rotating left by 7
+//        // can get 63.98 with:
+//        // adding 0x9E3779B9 for A and rotating left by 2
+//        // multiplying by 0xACED, NOTing, and rotating left by 28 for B
+//        BigInteger result = BigInteger.valueOf(0xFFF8A98DL), tmp = BigInteger.valueOf(0xFFFC9E3EL);
+//        result = tmp.divide(result.gcd(tmp)).multiply(result);
+////        tmp = BigInteger.valueOf(0xFFD299CBL);
+////        result = tmp.divide(result.gcd(tmp)).multiply(result);
+////        tmp = BigInteger.valueOf(0xFFABD755L);
+////        result = tmp.divide(result.gcd(tmp)).multiply(result);
+//        System.out.printf("\n0x%s, %2.6f\n", result.toString(16), Math.log(result.doubleValue()) / Math.log(2));
+//        int stateA = 1, i;
+//        for (int c = 1; c <= 200; c++) {
+//            final int r = Light32RNG.determine(5007 + c) | 0x80000001;
+//            //System.out.printf("(x ^ x << %d) + 0xC68E9CB7\n", c);
+//            System.out.printf("%03d/200, testing r = 0x%08X\n", c, r);
+//            for (int j = 1; j < 32; j++) {
+//                i = 0;
+//                for (; ; i++) {
+//                    if ((stateA = Integer.rotateLeft(stateA + 0xC68E9CB7 ^ r, j)) == 1) {
+//                        if (i >>> 24 == 0xFF)
+//                            System.out.printf("(state + 0xC68E9CB7 ^ 0x%08X, rotation %02d: 0x%08X\n", r, j, i);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+////        int stateA = 1, i = 0;
+////        for (; ; i++) {
+////            if((stateA = Integer.rotateLeft(~(stateA * 0x9E37), 7)) == 1)
+////            {
+////                System.out.printf("0x%08X\n", i);
+////                break;
+////            }
+////        }
+////        BigInteger result = BigInteger.valueOf(i & 0xFFFFFFFFL);
+////        i = 0;
+////        for (; ; i++) {
+////            if((stateA = Integer.rotateLeft(~(stateA * 0x4E6D), 17)) == 1)
+////            {
+////                System.out.printf("0x%08X\n", i);
+////                break;
+////            }
+////        }         
+////        BigInteger tmp = BigInteger.valueOf(i & 0xFFFFFFFFL);
+////        result = tmp.divide(result.gcd(tmp)).multiply(result);
+////        System.out.printf("\n0x%016X\n", result.longValue());
+//
+//    }
+//    
+////    public static void main(String[] args)
+////    {
+////        Mover32RNG m = new Mover32RNG();
+////        System.out.println("int[] startingA = {");
+////        for (int i = 0, ctr = 0; ctr < 128; ctr++, i+= 0x00000200) {
+////            m.setState(i);
+////            System.out.printf("0x%08X, ", m.stateA);
+////            if((ctr & 7) == 7)
+////                System.out.println();
+////        }
+////        System.out.println("}, startingB = {");
+////        for (int i = 0, ctr = 0; ctr < 128; ctr++, i+= 0x02000000) {
+////            m.setState(i);
+////            System.out.printf("0x%08X, ", m.stateB);
+////            if((ctr & 7) == 7)
+////                System.out.println();
+////        }
+////        System.out.println("};");
+////    }
 }
