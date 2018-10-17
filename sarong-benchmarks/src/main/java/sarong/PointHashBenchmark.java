23d17df5aeeae740849f1a9dbfbbe12becef2e0e
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-10-16 19:19:44 -0700

    Update PointHashBenchmark, Golden algo

--- sarong-benchmarks/src/main/java/sarong/PointHashBenchmark.java
+++ sarong-benchmarks/src/main/java/sarong/PointHashBenchmark.java
@@ -39,16 +39,36 @@ import java.util.concurrent.TimeUnit;
  * Benchmarks of hashes that take a fixed number of arguments, usually for geometric things like Perlin noise.
  * <pre>
  * Benchmark                                    Mode  Cnt   Score   Error  Units
- * PointHashBenchmark.measureGoldenPointHash2   avgt    5   6.022 ± 0.123  ns/op
- * PointHashBenchmark.measureHastyPointHash2    avgt    5   8.565 ± 0.051  ns/op
- * PointHashBenchmark.measurePointHash2         avgt    5   6.997 ± 0.061  ns/op
- * PointHashBenchmark.measureSzudzikPointHash2  avgt    5  12.069 ± 0.044  ns/op
+ * PointHashBenchmark.measureCantorPointHash2   avgt    5   6.241 ± 0.025  ns/op
+ * PointHashBenchmark.measureCantorPointHash3   avgt    5   8.096 ± 0.155  ns/op
+ * PointHashBenchmark.measureCantorPointHash4   avgt    5  10.427 ± 0.136  ns/op
+ * PointHashBenchmark.measureCantorPointHash6   avgt    5  13.904 ± 0.213  ns/op
+ * PointHashBenchmark.measureGoldenPointHash2   avgt    5   5.978 ± 0.148  ns/op
+ * PointHashBenchmark.measureGoldenPointHash3   avgt    5   6.974 ± 0.043  ns/op
+ * PointHashBenchmark.measureGoldenPointHash4   avgt    5   8.201 ± 0.179  ns/op
+ * PointHashBenchmark.measureGoldenPointHash6   avgt    5  11.242 ± 0.171  ns/op
+ * PointHashBenchmark.measureHastyPointHash2    avgt    5   8.498 ± 0.175  ns/op
+ * PointHashBenchmark.measureHastyPointHash3    avgt    5  11.083 ± 0.114  ns/op
+ * PointHashBenchmark.measureHastyPointHash4    avgt    5  13.417 ± 1.215  ns/op
+ * PointHashBenchmark.measureHastyPointHash6    avgt    5  19.454 ± 0.100  ns/op
+ * PointHashBenchmark.measurePointHash2         avgt    5   6.932 ± 0.217  ns/op
+ * PointHashBenchmark.measurePointHash3         avgt    5   8.551 ± 0.234  ns/op
+ * PointHashBenchmark.measurePointHash4         avgt    5  10.112 ± 0.068  ns/op
+ * PointHashBenchmark.measurePointHash6         avgt    5  13.027 ± 0.215  ns/op
+ * PointHashBenchmark.measureSzudzikPointHash2  avgt    5  11.974 ± 0.249  ns/op
+ * PointHashBenchmark.measureSzudzikPointHash3  avgt    5  18.125 ± 0.084  ns/op
+ * PointHashBenchmark.measureSzudzikPointHash4  avgt    5  23.321 ± 0.584  ns/op
+ * PointHashBenchmark.measureSzudzikPointHash6  avgt    5  33.887 ± 0.413  ns/op
  * </pre>
- * This benchmark just shows the 2D hashes (2 parameters and also a state parameter). The ranking of fastest to slowest
- * doesn't change for higher dimensions, though the Szudzik hashes slow down a lot more than the others with extra
- * dimensions (they're always in last place, so it doesn't matter anyway). Golden seems to be surprisingly fast, and I
- * wasn't really considering its algorithm seriously until I saw these benchmarks. PointHash is also surprisingly fast,
- * considering how much more code it is than Golden.
+ * This benchmark shows the time for each hash in nanoseconds per op (the second decimal is the range of error).
+ * GoldenPointHash is the fastest, PointHash and CantorPointHash are in a surprising tie for second place, and the Hasty
+ * and Szudzik point hashes don't offer much here, in the last two places. The Szudzik hashes slow down a lot more than
+ * the others with extra dimensions (they're always in last place, so it doesn't matter anyway). Golden seems to be
+ * surprisingly fast, and I wasn't really considering its algorithm seriously until I saw these benchmarks. PointHash is
+ * also surprisingly fast, considering how much more code it is than Golden. Cantor potentially is better as an actual
+ * hash-table hash function because it involves a perfect hash function, but it is still a little slower than Golden.
+ * Cantor also may possibly be symmetrical across the line {@code y = -x} in 2D, and similar hyperplanes in higher
+ * dimensions. Golden has replaced HastyPointHash (the one here) in SquidLib, and some noise functions have seen gains.
  */
 
 @State(Scope.Thread)
@@ -320,71 +340,191 @@ public class PointHashBenchmark {
 
     public static final class GoldenPointHash {
         /**
-         * @param x
-         * @param y
-         * @param s
+         * Gets a 64-bit point hash of a 2D point (x and y are both longs) and a state/seed as a long. This point
+         * hash has just about the best speed of any algorithms tested, and though its quality is mediocre for
+         * traditional uses of hashing (such as hash tables), it's sufficiently random to act as a positional RNG.
+         * <br>
+         * This uses a technique related to the one used by Martin Roberts for his golden-ratio-based sub-random
+         * sequences, where each axis is multiplied by a different constant, and the choice of constants depends on the
+         * number of axes but is always related to a generalized form of golden ratios, repeatedly dividing 1.0 by the
+         * generalized ratio. See
+         * <a href="http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/">Roberts' article</a>
+         * for some more information on how he uses this, but we do things differently because we want random-seeming
+         * results instead of separated sub-random results.
+         * @param x x position; any long
+         * @param y y position; any long
+         * @param s the state/seed; any long
          * @return 64-bit hash of the x,y point with the given state
          */
         public static long hashAll(long x, long y, long s) {
             y += s * 0xD1B54A32D192ED03L;
             x += y * 0xABC98388FB8FAC03L;
             s += x * 0x8CB92BA72F3D8DD7L;
-            return ((s = (s ^ s >> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
+            return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
 
         /**
-         * @param x
-         * @param y
-         * @param z
-         * @param s
+         * Gets a 64-bit point hash of a 3D point (x, y, and z are all longs) and a state/seed as a long. This point
+         * hash has just about the best speed of any algorithms tested, and though its quality is mediocre for
+         * traditional uses of hashing (such as hash tables), it's sufficiently random to act as a positional RNG.
+         * <br>
+         * This uses a technique related to the one used by Martin Roberts for his golden-ratio-based sub-random
+         * sequences, where each axis is multiplied by a different constant, and the choice of constants depends on the
+         * number of axes but is always related to a generalized form of golden ratios, repeatedly dividing 1.0 by the
+         * generalized ratio. See
+         * <a href="http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/">Roberts' article</a>
+         * for some more information on how he uses this, but we do things differently because we want random-seeming
+         * results instead of separated sub-random results.
+         * @param x x position; any long
+         * @param y y position; any long
+         * @param z z position; any long
+         * @param s the state/seed; any long
          * @return 64-bit hash of the x,y,z point with the given state
          */
         public static long hashAll(long x, long y, long z, long s) {
             z += s * 0xDB4F0B9175AE2165L;
             y += z * 0xBBE0563303A4615FL;
             x += y * 0xA0F2EC75A1FE1575L;
-            s += x * 0x89E182857D9ED688L;
-            return ((s = (s ^ s >> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
+            s += x * 0x89E182857D9ED689L;
+            return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
 
         /**
-         * @param x
-         * @param y
-         * @param z
-         * @param w
-         * @param s
+         * Gets a 64-bit point hash of a 4D point (x, y, z, and w are all longs) and a state/seed as a long. This point
+         * hash has just about the best speed of any algorithms tested, and though its quality is mediocre for
+         * traditional uses of hashing (such as hash tables), it's sufficiently random to act as a positional RNG.
+         * <br>
+         * This uses a technique related to the one used by Martin Roberts for his golden-ratio-based sub-random
+         * sequences, where each axis is multiplied by a different constant, and the choice of constants depends on the
+         * number of axes but is always related to a generalized form of golden ratios, repeatedly dividing 1.0 by the
+         * generalized ratio. See
+         * <a href="http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/">Roberts' article</a>
+         * for some more information on how he uses this, but we do things differently because we want random-seeming
+         * results instead of separated sub-random results.
+         * @param x x position; any long
+         * @param y y position; any long
+         * @param z z position; any long
+         * @param w w position; any long
+         * @param s the state; any long
          * @return 64-bit hash of the x,y,z,w point with the given state
          */
         public static long hashAll(long x, long y, long z, long w, long s) {
             w += s * 0xE19B01AA9D42C633L;
             z += w * 0xC6D1D6C8ED0C9631L;
             y += z * 0xAF36D01EF7518DBBL;
-            x += y * 0x9A69443F36F710E6L;
+            x += y * 0x9A69443F36F710E7L;
             s += x * 0x881403B9339BD42DL;
-            return ((s = (s ^ s >> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
+            return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
 
         /**
-         * @param x
-         * @param y
-         * @param z
-         * @param w
-         * @param u
-         * @param v
-         * @param s
+         * Gets a 64-bit point hash of a 6D point (x, y, z, w, u, and v are all longs) and a state/seed as a long. This
+         * point hash has just about the best speed of any algorithms tested, and though its quality is mediocre for
+         * traditional uses of hashing (such as hash tables), it's sufficiently random to act as a positional RNG.
+         * <br>
+         * This uses a technique related to the one used by Martin Roberts for his golden-ratio-based sub-random
+         * sequences, where each axis is multiplied by a different constant, and the choice of constants depends on the
+         * number of axes but is always related to a generalized form of golden ratios, repeatedly dividing 1.0 by the
+         * generalized ratio. See
+         * <a href="http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/">Roberts' article</a>
+         * for some more information on how he uses this, but we do things differently because we want random-seeming
+         * results instead of separated sub-random results.
+         * @param x x position; any long
+         * @param y y position; any long
+         * @param z z position; any long
+         * @param w w position; any long
+         * @param u u position; any long
+         * @param v v position; any long
+         * @param s the state; any long
          * @return 64-bit hash of the x,y,z,w,u,v point with the given state
          */
         public static long hashAll(long x, long y, long z, long w, long u, long v, long s) {
             v += s * 0xE95E1DD17D35800DL;
-            u += v * 0xD4BC74E13F3C782EL;
-            w += u * 0xC1EDBC5B5C68AC24L;
-            z += w * 0xB0C8AC50F0EDEF5CL;
+            u += v * 0xD4BC74E13F3C782FL;
+            w += u * 0xC1EDBC5B5C68AC25L;
+            z += w * 0xB0C8AC50F0EDEF5DL;
             y += z * 0xA127A31C56D1CDB5L;
-            x += y * 0x92E852C80D153DB2L;
+            x += y * 0x92E852C80D153DB3L;
             s += x * 0x85EB75C3024385C3L;
-            return ((s = (s ^ s >> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
+            return ((s = (s ^ s >>> 27 ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5CC83L) ^ s >>> 25);
         }
     }
+
+    public static final class CantorPointHash {
+        /**
+         * Gets a 64-bit point hash of a 3D point (x and y are both longs) and a state/seed as a long.
+         * This uses the 2D Cantor tuple function, then feeds that (very non-random) perfect hash through a simple
+         * RNG-like unary hash (it is close to SquidLib's old ThrustRNG).
+         * @param x x position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param y y position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param state any long
+         * @return 64-bit hash of the x,y,z point with the given state
+         */
+        public static long hashAll(long x, long y, long state)
+        {
+            x += ((((x+y) * (x+y+1) >> 1) + y) ^ state) * 0xC13FA9A902A6328FL;
+            return (x = (x ^ x >>> 25) * 0x9E3779B97F4A7C15L) ^ (x >>> 22);
+        }
+        /**
+         * Gets a 64-bit point hash of a 3D point (x, y, and z are all longs) and a state/seed as a long.
+         * This uses the 3D Cantor tuple function, then feeds that (very non-random) perfect hash through a simple
+         * RNG-like unary hash (it is close to SquidLib's old ThrustRNG).
+         * @param x x position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param y y position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param z z position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param state any long
+         * @return 64-bit hash of the x,y,z point with the given state
+         */
+        public static long hashAll(long x, long y, long z, long state)
+        {
+            y += (((y+z) * (y+z+1) >> 1) + z);
+            x += ((((x+y) * (x+y+1) >> 1) + y) ^ state) * 0xC13FA9A902A6328FL;
+            return (x = (x ^ x >>> 25) * 0x9E3779B97F4A7C15L) ^ (x >>> 22);
+        }
+
+        /**
+         * Gets a 64-bit point hash of a 4D point (x, y, z, and w are all longs) and a state/seed as a long.
+         * This uses the 3D Cantor tuple function, then feeds that (very non-random) perfect hash through a simple
+         * RNG-like unary hash (it is close to SquidLib's old ThrustRNG).
+         * @param x x position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param y y position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param z z position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param w w position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param state any long
+         * @return 64-bit hash of the x,y,z,w point with the given state
+         */
+        public static long hashAll(long x, long y, long z, long w, long state)
+        {
+            z += (((z+w) * (z+w+1) >> 1) + w);
+            y += (((y+z) * (y+z+1) >> 1) + z);
+            x += ((((x+y) * (x+y+1) >> 1) + y) ^ state) * 0xC13FA9A902A6328FL;
+            return (x = (x ^ x >>> 25) * 0x9E3779B97F4A7C15L) ^ (x >>> 22);
+        }
+
+        /**
+         * Gets a 64-bit point hash of a 6D point (x, y, z, w, u, and v are all longs) and a state/seed as a long.
+         * This uses the 6D Cantor tuple function, then feeds that (very non-random) perfect hash through a simple
+         * RNG-like unary hash (it is close to SquidLib's old ThrustRNG).
+         * @param x x position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param y y position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param z z position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param w w position, as a non-negative long (it may work for negative longs, but no guarantees)
+         * @param state any long
+         * @return 64-bit hash of the x,y,z,w point with the given state
+         */
+        public static long hashAll(long x, long y, long z, long w, long u, long v, long state)
+        {
+            u += (((u+v) * (u+v+1) >> 1) + v);
+            w += (((w+u) * (w+u+1) >> 1) + u);
+            z += (((z+w) * (z+w+1) >> 1) + w);
+            y += (((y+z) * (y+z+1) >> 1) + z);
+            x += ((((x+y) * (x+y+1) >> 1) + y) ^ state) * 0xC13FA9A902A6328FL;
+            return (x = (x ^ x >>> 25) * 0x9E3779B97F4A7C15L) ^ (x >>> 22);
+        }
+    }
+
+
+
     @Benchmark
     public long measurePointHash2()
     {
@@ -472,5 +612,26 @@ public class PointHashBenchmark {
                 inputs[u++ & 0xFFFF], inputs[v++ & 0xFFFF], 1L);
     }
 
+    @Benchmark
+    public long measureCantorPointHash2()
+    {
+        return CantorPointHash.hashAll(inputs[x++ & 0xFFFF], inputs[y++ & 0xFFFF], 1L);
+    }
+    @Benchmark
+    public long measureCantorPointHash3()
+    {
+        return CantorPointHash.hashAll(inputs[x++ & 0xFFFF], inputs[y++ & 0xFFFF], inputs[z++ & 0xFFFF], 1L);
+    }
+    @Benchmark
+    public long measureCantorPointHash4()
+    {
+        return CantorPointHash.hashAll(inputs[x++ & 0xFFFF], inputs[y++ & 0xFFFF], inputs[z++ & 0xFFFF], inputs[w++ & 0xFFFF], 1L);
+    }
+    @Benchmark
+    public long measureCantorPointHash6()
+    {
+        return CantorPointHash.hashAll(inputs[x++ & 0xFFFF], inputs[y++ & 0xFFFF], inputs[z++ & 0xFFFF], inputs[w++ & 0xFFFF],
+                inputs[u++ & 0xFFFF], inputs[v++ & 0xFFFF], 1L);
+    }
 
 }
