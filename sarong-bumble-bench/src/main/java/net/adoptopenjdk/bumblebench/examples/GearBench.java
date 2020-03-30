22c1f5dd16e0965564f04be85da01bd563ba089c
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-03-29 19:37:00 -0700

    Add new, very fast, high-quality, good-period GearRNG; update TangleRNG

---
+++ sarong-bumble-bench/src/main/java/net/adoptopenjdk/bumblebench/examples/GearBench.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+import sarong.GearRNG;
+
+/**
+ * (On that slower laptop, Windows 7, AdoptOpenJDK 8):
+ * <br>
+ * GearBench score: 785985920.000000 (786.0M 2048.2%)
+ *       uncertainty:   0.3%
+ * <br>
+ * This is quite a bit faster than Orbit or Tangle on the same old machine!
+ * I need to bench all three on the newer machine to get a fair comparison.
+ */
+public final class GearBench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		GearRNG rng = new GearRNG(0x12345678);
+		long sum = 0L;
+		for (long i = 0; i < numIterations; i++)
+			sum += rng.nextLong();
+		return numIterations;
+	}
+}
+
--- src/main/java/sarong/TangleRNG.java
+++ src/main/java/sarong/TangleRNG.java
@@ -119,7 +119,7 @@ public final class TangleRNG implements RandomnessSource, SkippingRandomness, Se
     public final int next(final int bits) {
         final long s = (stateA += 0xC6BC279692B5C323L);
         final long z = (s ^ s >>> 31) * (stateB += 0x9E3779B97F4A7C16L);
-        return (int)(z ^ z >>> 26) >>> (32 - bits);
+        return (int)(z ^ z >>> 26 ^ z >>> 6) >>> (32 - bits);
     }
     /**
      * Using this method, any algorithm that needs to efficiently generate more
@@ -133,7 +133,7 @@ public final class TangleRNG implements RandomnessSource, SkippingRandomness, Se
     public final long nextLong() {
         final long s = (stateA += 0xC6BC279692B5C323L);
         final long z = (s ^ s >>> 31) * (stateB += 0x9E3779B97F4A7C16L);
-        return z ^ z >>> 26;
+        return z ^ z >>> 26 ^ z >>> 6;
     }
 
     /**
@@ -180,7 +180,7 @@ public final class TangleRNG implements RandomnessSource, SkippingRandomness, Se
     public long skip(long advance) {
         final long s = (stateA += 0xC6BC279692B5C323L * advance);
         final long z = (s ^ s >>> 31) * (stateB += 0x9E3779B97F4A7C16L * advance);
-        return z ^ z >>> 26;
+        return z ^ z >>> 26 ^ z >>> 6;
     }
 
     /**
