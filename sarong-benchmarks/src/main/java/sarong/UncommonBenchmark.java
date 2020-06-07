0cc8e5e9cd60e7216e10331d725d99a632126bc0
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-06-07 16:58:22 -0700

    Fix the JMH benchmarks project build

--- sarong-benchmarks/src/main/java/sarong/UncommonBenchmark.java
+++ sarong-benchmarks/src/main/java/sarong/UncommonBenchmark.java
@@ -37,6 +37,7 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
+import sarong.discouraged.*;
 
 import java.util.concurrent.TimeUnit;
 
