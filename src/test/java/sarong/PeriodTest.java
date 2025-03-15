8095f771669fa5e84a4a031fca45b9e0e6c599ea
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-03-15 00:03:29 -0700

    Check a CountingByXoshiro4x5...

--- src/test/java/sarong/PeriodTest.java
+++ src/test/java/sarong/PeriodTest.java
@@ -299,6 +299,43 @@ public class PeriodTest {
         System.out.printf("Best left shift was %d, best left rotation was %d, with period 0x%06X\nTook %d ms.\n",
                 bestShift, bestRot, best, (System.currentTimeMillis() - startTime));
     }
+    /**
+     * Period was 0x01FFFFE0
+     * Took 2005 ms.
+     * 33554400/33554432 outputs were present.
+     * 9.5367431640625E-5% of outputs were missing.
+     * 0 0 0 0 0
+     * 0 0 0 0 1
+     * 0 0 0 0 2
+     * 0 0 0 0 3
+     * 0 0 0 0 4
+     */
+    @Test
+    public void checkPeriodCountingByXoshiro4x5(){
+        long startTime = System.currentTimeMillis();
+        final RoaringBitmap all = new RoaringBitmap();
+        int stateA = 1, stateB = 1, stateC = 1, stateD = 1;
+        int stateE = 1;
+        long i = 0L;
+        while (++i <= 0x10000100L) {
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateE = stateE + ~(stateB ^= stateC) & 31;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            all.add(stateA | stateB << 5 | stateC << 10 | stateD << 15 | stateE << 20);
+            if (stateA == 1 && stateB == 1 && stateC == 1 && stateD == 1 && stateE == 1) {
+                break;
+            }
+        }
+        System.out.printf("Period was 0x%08X\nTook %d ms.\n", i, (System.currentTimeMillis() - startTime));
+        System.out.println(all.getLongCardinality() + "/" + (1 << 25) + " outputs were present.");
+        System.out.println(100.0 - all.getLongCardinality() * 0x64p-25 + "% of outputs were missing.");
+        all.flip(0L, 1L << 25);
+        all.forEach((int ii) -> System.out.printf("%d %d %d %d %d\n", ii & 31, ii>>>5 & 31, ii>>>10 & 31, ii>>>15 & 31, ii>>>20 & 31));
+    }
 
     /**
      * Best right shift was 1, best left rotation was 1, with period 3FFFFF
