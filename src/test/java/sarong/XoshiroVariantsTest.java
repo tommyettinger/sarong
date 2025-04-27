27e1ceabc758355f761fd6ae71fd6d9ddc549cfe
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-04-26 21:23:47 -0700

    Still "almost equi" for 3D and 2D, but not for 1D!
    1D equidistributed exactly!

--- src/test/java/sarong/XoshiroVariantsTest.java
+++ src/test/java/sarong/XoshiroVariantsTest.java
@@ -184,4 +184,204 @@ public class XoshiroVariantsTest {
         System.out.println("Number of repetitions of a 4-tuple to the number of 4-tuples that repeated that often:");
         System.out.println(inv.toString(" 4-tuples.\n", " repetitions occurred for ", false, Base::appendReadable, Base::appendReadable) + " 4-tuples.");
     }
+    /**
+     * <pre>
+     * Period was 0x3FFFFC00
+     * Took 6688 ms.
+     * 32768/32768 3-tuples were present.
+     * 0.0% of 3-tuples were missing.
+     * Number of repetitions of a 3-tuple to the number of 3-tuples that repeated that often:
+     * 32764 repetitions occurred for 2 3-tuples.
+     * 32765 repetitions occurred for 6 3-tuples.
+     * 32766 repetitions occurred for 74 3-tuples.
+     * 32767 repetitions occurred for 850 3-tuples.
+     * 32768 repetitions occurred for 31836 3-tuples.
+     * </pre>
+     */
+    @Test
+    public void check3TupleFrequencyOrbitalByXoshiro4x5() {
+        long startTime = System.currentTimeMillis();
+        final IntIntMap all = new IntIntMap(1 << 15, 0.6f);
+        int stateA = 1, stateB = 1, stateC = 1, stateD = 1, stateE = 1, stateF = 1;
+        int joined = 0;
+        for (int g = 0; g < 20; g++) {
+            int result = ((stateE << 4 | stateE >> 1) & 31) ^ stateF;
+            stateE = stateE + 0x1D + stateC & 31;
+            stateF = stateF + 0x17 + clz5(stateE) & 31;
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateB ^= stateC;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            joined = (joined << 5 & 0x0007FE0) | result;
+        }
+        int endA = stateA, endB = stateB, endC = stateC, endD = stateD, endE = stateE, endF = stateF;
+
+        long i = 0L;
+        while (++i <= 0x40000100L) {
+            int result = ((stateE << 4 | stateE >> 1) & 31) ^ stateF;
+            stateE = stateE + 0x1D + stateC & 31;
+            stateF = stateF + 0x17 + clz5(stateE) & 31;
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateB ^= stateC;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            all.getAndIncrement((joined = (joined << 5 & 0x0007FE0) | result), 0, 1);
+            if (stateA == endA && stateB == endB && stateC == endC && stateD == endD && stateE == endE && stateF == endF) {
+                break;
+            }
+        }
+
+        System.out.printf("Period was 0x%08X\nTook %d ms.\n", i, (System.currentTimeMillis() - startTime));
+        System.out.println(all.size() + "/" + (1 << 15) + " 3-tuples were present.");
+        System.out.println(100.0 - all.size() * 0x64p-15 + "% of 3-tuples were missing.");
+        IntIntOrderedMap inv = new IntIntOrderedMap(400, 0.6f);
+        for(IntIntMap.Entry ent : all){
+            inv.getAndIncrement(ent.value, 0, 1);
+        }
+        inv.sort(IntComparators.NATURAL_COMPARATOR);
+        System.out.println("Number of repetitions of a 3-tuple to the number of 3-tuples that repeated that often:");
+        System.out.println(inv.toString(" 3-tuples.\n", " repetitions occurred for ", false, Base::appendReadable, Base::appendReadable) + " 3-tuples.");
+    }
+    /**
+     * <pre>
+     * Period was 0x3FFFFC00
+     * Took 5674 ms.
+     * 1024/1024 2-tuples were present.
+     * 0.0% of 2-tuples were missing.
+     * Number of repetitions of a 2-tuple to the number of 2-tuples that repeated that often:
+     * 1048568 repetitions occurred for 2 2-tuples.
+     * 1048569 repetitions occurred for 2 2-tuples.
+     * 1048570 repetitions occurred for 4 2-tuples.
+     * 1048571 repetitions occurred for 4 2-tuples.
+     * 1048572 repetitions occurred for 40 2-tuples.
+     * 1048573 repetitions occurred for 42 2-tuples.
+     * 1048574 repetitions occurred for 200 2-tuples.
+     * 1048575 repetitions occurred for 264 2-tuples.
+     * 1048576 repetitions occurred for 466 2-tuples.
+     * </pre>
+     */
+    @Test
+    public void check2TupleFrequencyOrbitalByXoshiro4x5() {
+        long startTime = System.currentTimeMillis();
+        final IntIntMap all = new IntIntMap(1 << 10, 0.6f);
+
+        int stateA = 1, stateB = 1, stateC = 1, stateD = 1, stateE = 1, stateF = 1;
+        int joined = 0;
+        for (int g = 0; g < 20; g++) {
+            int result = ((stateE << 4 | stateE >> 1) & 31) ^ stateF;
+            stateE = stateE + 0x1D + stateC & 31;
+            stateF = stateF + 0x17 + clz5(stateE) & 31;
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateB ^= stateC;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            joined = (joined << 5 & 0x000003E0) | result;
+        }
+        int endA = stateA, endB = stateB, endC = stateC, endD = stateD, endE = stateE, endF = stateF;
+
+        long i = 0L;
+        while (++i <= 0x40000100L) {
+            int result = ((stateE << 4 | stateE >> 1) & 31) ^ stateF;
+            stateE = stateE + 0x1D + stateC & 31;
+            stateF = stateF + 0x17 + clz5(stateE) & 31;
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateB ^= stateC;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            all.getAndIncrement((joined = (joined << 5 & 0x000003E0) | result), 0, 1);
+            if (stateA == endA && stateB == endB && stateC == endC && stateD == endD && stateE == endE && stateF == endF) {
+                break;
+            }
+        }
+        System.out.printf("Period was 0x%08X\nTook %d ms.\n", i, (System.currentTimeMillis() - startTime));
+        System.out.println(all.size() + "/" + (1 << 10) + " 2-tuples were present.");
+        System.out.println(100.0 - all.size() * 0x64p-10 + "% of 2-tuples were missing.");
+        IntIntOrderedMap inv = new IntIntOrderedMap(1000, 0.6f);
+        for(IntIntMap.Entry ent : all){
+            inv.getAndIncrement(ent.value, 0, 1);
+        }
+        inv.sort(IntComparators.NATURAL_COMPARATOR);
+        System.out.println("Number of repetitions of a 2-tuple to the number of 2-tuples that repeated that often:");
+        System.out.println(inv.toString(" 2-tuples.\n", " repetitions occurred for ", false, Base::appendReadable, Base::appendReadable) + " 2-tuples.");
+    }
+    /**
+     * <pre>
+     * Period was 0x3FFFFC00
+     * Took 3811 ms.
+     * Frequency of 00: 33554400
+     * Frequency of 01: 33554400
+     * Frequency of 02: 33554400
+     * Frequency of 03: 33554400
+     * Frequency of 04: 33554400
+     * Frequency of 05: 33554400
+     * Frequency of 06: 33554400
+     * Frequency of 07: 33554400
+     * Frequency of 08: 33554400
+     * Frequency of 09: 33554400
+     * Frequency of 10: 33554400
+     * Frequency of 11: 33554400
+     * Frequency of 12: 33554400
+     * Frequency of 13: 33554400
+     * Frequency of 14: 33554400
+     * Frequency of 15: 33554400
+     * Frequency of 16: 33554400
+     * Frequency of 17: 33554400
+     * Frequency of 18: 33554400
+     * Frequency of 19: 33554400
+     * Frequency of 20: 33554400
+     * Frequency of 21: 33554400
+     * Frequency of 22: 33554400
+     * Frequency of 23: 33554400
+     * Frequency of 24: 33554400
+     * Frequency of 25: 33554400
+     * Frequency of 26: 33554400
+     * Frequency of 27: 33554400
+     * Frequency of 28: 33554400
+     * Frequency of 29: 33554400
+     * Frequency of 30: 33554400
+     * Frequency of 31: 33554400
+     * </pre>
+     */
+    @Test
+    public void checkFrequencyOrbitalByXoshiro4x5() {
+        long startTime = System.currentTimeMillis();
+        final int[] frequencies = new int[32];
+        int stateA = 1, stateB = 1, stateC = 1, stateD = 1, stateE = 1, stateF = 1;
+        int endA = stateA, endB = stateB, endC = stateC, endD = stateD, endE = stateE, endF = stateF;
+
+        long i = 0L;
+        while (++i <= 0x40000100L) {
+            int result = ((stateE << 4 | stateE >> 1) & 31) ^ stateF;
+            stateE = stateE + 0x1D + stateC & 31;
+            stateF = stateF + 0x17 + clz5(stateE) & 31;
+            int t = stateB >>> 1;
+            stateC ^= stateA;
+            stateD ^= stateB;
+            stateB ^= stateC;
+            stateA ^= stateD;
+            stateC ^= t;
+            stateD = (stateD << 2 | stateD >>> 3) & 31;
+            frequencies[result]++;
+            if (stateA == endA && stateB == endB && stateC == endC && stateD == endD && stateE == endE && stateF == endF) {
+                break;
+            }
+        }
+        System.out.printf("Period was 0x%08X\nTook %d ms.\n", i, (System.currentTimeMillis() - startTime));
+        for (int j = 0; j < 32; j++) {
+            System.out.printf("Frequency of %02d: %d \n", j, frequencies[j]);
+        }
+    }
+
 }
